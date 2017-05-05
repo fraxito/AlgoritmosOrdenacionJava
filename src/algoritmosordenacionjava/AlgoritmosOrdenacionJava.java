45faa368c5622927afb3537b8af279a74f7713c7
jorgecisneros <jorgecisneros@10.10.31.101>
2017-05-05 11:33:19 +0200

    Con la Burbuja

---
+++ src/algoritmosordenacionjava/AlgoritmosOrdenacionJava.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritmosordenacionjava;
+
+/**
+ *
+ * @author jorgecisneros
+ */
+public class AlgoritmosOrdenacionJava {
+
+    public void imprimeArray( int [ ] numeros ){
+        for(int i=0;  i < numeros.length;  i++ ){
+            System.out.print(numeros[ i ] + " ");
+        } 
+    } 
+       
+    public void ordenacionBurbuja( int [ ] numeros ){
+        int aux;   //variable
+        for (int j=1; j<numeros.length; j++){
+            for(int i=0;  i < numeros.length -j;  i++ ){
+                if ( numeros[ i ] > numeros[i+1] ) {
+                    aux = numeros[ i ];                //intercambia los elementos
+                    numeros[ i ] = numeros[ i+1 ];
+                    numeros[ i+1 ] = aux; 
+                } 
+            } 
+        }
+    } 
+ 
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int [] listaNumeros = {57,22, 11, 13, 21, 7};
+        AlgoritmosOrdenacionJava ejercicio1 = new AlgoritmosOrdenacionJava();
+        ejercicio1.ordenacionBurbuja(listaNumeros);
+        ejercicio1.imprimeArray(listaNumeros);
+        
+    }
+    
+}
