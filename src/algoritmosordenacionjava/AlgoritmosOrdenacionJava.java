1e96bd6647daa79ec9e7b5af602232ae568d9df0
jorgecisneros <jorgecisneros@10.10.31.101>
2017-05-12 08:20:53 +0200

    Con QuickSort

--- src/algoritmosordenacionjava/AlgoritmosOrdenacionJava.java
+++ src/algoritmosordenacionjava/AlgoritmosOrdenacionJava.java
@@ -71,6 +71,42 @@ public class AlgoritmosOrdenacionJava {
 
     }
     
+    public void quickSort(int[] numeros, int izquierda, int derecha) {
+         
+        int i = izquierda;
+        int j = derecha;
+        // toma como pivote el primer elemento de la izquierda
+        int pivot = numeros[izquierda];
+ 
+        while (i <= j) {
+                //en cada pasada del bucle se busca un número mayor que el pivote por la izquierda y un numero menor que 
+                // el pivote por la derecha. Si los encuentra, los intercambia
+            while (numeros[i] < pivot) {
+                i++;
+            }
+            while (numeros[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                int aux = numeros[i];
+                numeros[i] = numeros[j];
+                numeros[j] = aux;
+                //actualiza los indices por la derecha y por la izquierda
+                i++;
+                j--;
+            }
+        }
+        // llama a  quickSort() de forma recursiva con los dos subarrays que han quedado
+        if (izquierda < j){
+            quickSort(numeros,izquierda, j);
+        }
+        if (i < derecha){
+            quickSort(numeros,i, derecha);
+        }
+    }
+
+
+    
     /**
      * @param args the command line arguments
      */
@@ -80,7 +116,10 @@ public class AlgoritmosOrdenacionJava {
         //ejercicio1.ordenacionBurbuja(listaNumeros);
         //ejercicio1.ordenacionBurbujaConWhile(listaNumeros);
         //ejercicio1.insercionDirecta(listaNumeros);
-        ejercicio1.insercionDirectaWhile(listaNumeros);
+        //ejercicio1.insercionDirectaWhile(listaNumeros);
+        
+        ejercicio1.quickSort(listaNumeros, 0, listaNumeros.length-1);
+        
         ejercicio1.imprimeArray(listaNumeros);
         
     }
